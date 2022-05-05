f14a6a7f766bf12d35d5807310402aba5ac4d341
Christian Scholtysik <christianscholtysik@gmail.com>
2022-05-05 17:53:30 +0200

    add new header styling + bootstrap card element

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-const Header = ({ post }) => {
+export default function Header({ post }) {
   //  const newestPost = posts.push(posts.length - 1);
   //  const newestPost = posts.push(
   //    (post) => student.login.uuid === id
   //  );
   // console.log(post);
+
   return (
-    <div className="headerContent">
-      <hr className="solid" />
-      <div id="img-div">
+    //     <div className="headerContent">
+    //       <hr className="solid" />
+    //       <div id="img-div">
+    //         <img
+    //           src={post.fields.image.fields.file.url}
+    //           alt=""
+    //           width="300"
+    //           height="400"
+    //         />
+    //       </div>
+    //       <Link to={`../${post.sys.id}`}>
+    //         <h2>{post.fields.shortDescription}</h2>
+    //       </Link>
+    //       <hr className="solid" />
+    //     </div>
+    //   );
+    // };
+
+    <Card className="text-center">
+      <Card.Header>
+        <h2>Featured- the newest Article</h2>
+      </Card.Header>
+      <Card.Body className="header">
+        <Card.Title></Card.Title>
         <img
           src={post.fields.image.fields.file.url}
           alt=""
-          width="300"
+          width="400"
           height="400"
         />
-      </div>
-      <Link to={`../${post.sys.id}`}>
-        <h2>{post.fields.shortDescription}</h2>
-      </Link>
-      <hr className="solid" />
-    </div>
-  );
-};
+        <Card.Text>{post.fields.shortDescription}</Card.Text>
 
-export default Header;
+        <Link to={`../${post.sys.id}`}>
+          <button id="btn-card"> Read full article</button>
+        </Link>
+      </Card.Body>
+      <Card.Footer className="text-muted">{post.sys.createdAt}</Card.Footer>
+    </Card>
+  );
+}
